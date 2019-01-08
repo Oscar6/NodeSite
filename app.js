aa1230ebe6aee86c3928d98cf15666615f120965
Oscar6 <oscar.m1024@gmail.com>
2019-01-08 09:13:12 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('assets'));
+
+app.use(require('./routes/index.js'))
+app.use(require('./routes/site.js'))
+app.use(require('./routes/kendrick.js'))
+// app.use(require('./routes/thestrokes.js'))
+// app.use(require('./routes/party.js'))
+
+
+// Runs app by typing "nodemon nameOfFile.js"
+app.listen(3000, ()=> {
+    console.log('running on port 3000')
+})
